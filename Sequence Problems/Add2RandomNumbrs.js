44610c341fd6fc45362f5af3498e0580556d86e2
Hemantdhurve <hemantdhurve007@gmail.com>
2022-10-04 02:19:02 +0530

    [Hemant] Added 2 Dice numbers generated By Random Function

---
+++ Sequence Problems/Add2RandomNumbrs.js
@@ -0,0 +1,12 @@
+//Using Random Function To add 2 Numbers
+
+let Dicenum1=Math.floor(Math.random()*6)+1;      //random()*6)+1 used when it prints from 1 to 6
+console.log("First Number is :" + Dicenum1);
+
+let Dicenum2=Math.floor(Math.random()*6)+1;
+console.log("Second Number is :" + Dicenum2);
+
+let Addition=Dicenum1+Dicenum2;
+
+console.log("Addition of both numbers are : " +Addition);
+
