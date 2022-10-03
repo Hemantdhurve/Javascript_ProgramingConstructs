7675ca1310ffdee6a05a5fb1458857bb648d6c46
Hemantdhurve <hemantdhurve007@gmail.com>
2022-10-04 02:36:21 +0530

    [Hemant] Added Program to find Sum and Average of 5 Random Numbers

---
+++ Sequence Problems/TwoDigitRandomNum.js
@@ -0,0 +1,14 @@
+
+let num1=Math.floor(Math.random()*99)+10;
+let num2=Math.floor(Math.random()*99)+10;
+let num3=Math.floor(Math.random()*99)+10;
+let num4=Math.floor(Math.random()*99)+10;
+let num5=Math.floor(Math.random()*99)+10;
+
+//To Find Sum
+let Sum=num1+num2+num3+num4+num5;
+console.log("Sum of 5 numbers are :"+Sum);
+
+//To Find Average
+let Average=Sum/5;
+console.log("Average of the 5 number is: "+ Average);
