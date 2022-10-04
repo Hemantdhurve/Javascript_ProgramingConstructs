e82a4f64b7a953e0d304d7250ca9bd51d60c4c8b
Hemantdhurve <hemantdhurve007@gmail.com>
2022-10-04 15:09:38 +0530

    [Hemant] Added a Flip coin Problem

---
+++ If&Else Problems/Coinflip.js
@@ -0,0 +1,10 @@
+
+let num=Math.floor(Math.random()*2)+1;
+console.log("Generated Random Number is : " + num);
+if(num==1)
+{
+    console.log("After Flipping coin we get Head " );
+}
+else{
+    console.log("After Flipping coin we get Tail");
+}
