6342d017d64e90802c13959334c9a56f41abc761
Hemantdhurve <hemantdhurve007@gmail.com>
2022-10-05 13:49:09 +0530

    [Hemant] Added Problem of WeekDays Conversion

---
+++ If-ElseIf Problems/WeekDays.js
@@ -0,0 +1,31 @@
+let day=Math.floor(Math.random()*7)+1;
+console.log("Random Single Digit Number Generated is : "+day);
+
+if(day==1)
+{
+    console.log("On day 1 it assigns SUNDAY");
+}
+else if(day==2)
+{
+    console.log("On day 2 it assigns MONDAY");
+}
+else if(day==3)
+{
+    console.log("On day 3 it assigns TUESDAY");
+}
+else if(day==4)
+{
+    console.log("On day 4 it assigns WEDNESDAY");
+}
+else if(day==5)
+{
+    console.log("On day 5 it assigns THURSDAY");
+}
+else if(day==6)
+{
+    console.log("On day 6 it assigns FRIDAY");
+}
+else if(day==7)
+{
+    console.log("On day 7 it assigns SATURDAY");
+}
